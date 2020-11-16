3e7705f03a77ac569a3828a43afe2448a09a7ee8
sdisawal <disawalsagar>
2020-11-15 21:42:08 -0500

    Adding Visualization

--- snp_diff.py
+++ snp_diff.py
@@ -8,6 +8,7 @@ Created on Sun Nov  8 21:58:32 2020
 import sys
 sys.path.append(r'C:\Users\sdisawal\Desktop\Stocks\Code')
 sys.path.append(r'C:\Users\sdisawal\PycharmProjects\LearnApi\alpha_vantage')
+import plotly.express as px
 #print(sys.path)
 
 
@@ -73,10 +74,12 @@ t_df = pd.read_csv(r'C:\Users\sdisawal\Desktop\Stocks\Code\csv\stock_Values.csv'
 t_df = t_df.rename(columns= {'4. close' : 'close'}).reset_index(drop=True)
 
 #%%
-print(p_df.columns)
+#print(p_df.columns)
 p_df = p_df[['Stocks', 'Symbol', 'Quantity','Bought Price','Date']]
-#t_df = t_df[t_df['Ticker'] == 'VOO'][['Ticker','date','close']]
-df_merge1 = p_df.merge(t_df, left_on = ['Date'], right_on=['date'], how='left')
+p_df = p_df.rename(columns = {'Date': 'org_date'})
+t_df_VOO = t_df[t_df['Ticker'] == 'VOO'][['Ticker','date','close']]
+t_df_VOO = t_df_VOO.rename(columns = {'close': 'voo_close', 'Ticker': 'voo_Ticker'})
+df_merge1 = p_df.merge(t_df_VOO, left_on = ['org_date'], right_on=['date'], how='left')
 #df_merge1.rename(columns= {'Date' : 'Date_stock', },inplace = True)
 df_merge1 = df_merge1.drop('date', axis=1)
 
@@ -86,13 +89,51 @@ def lat_dts(df):
     df_srtd = df_1.groupby('Ticker').head(1).reset_index(drop=True)
     return df_srtd
 #%%
-t_df_srtd = lat_dts(t_df)
+t_df_srtd = lat_dts(t_df)[['date', 'close', 'Ticker']]
+t_df_srtd = t_df_srtd.rename(columns = {'date' : 'latest_date', 'close': 'latest_close'}).reset_index(drop=True)
 
 #%%
 df_merge = df_merge1.merge(t_df_srtd, left_on = ['Symbol'], right_on=['Ticker'], how='left')
-#df_merge['p/l'] = (df_merge['Bought Price'] - df_merge['close']) * df_merge.Quantity
+voo_latest_price = t_df_srtd.loc[t_df_srtd['Ticker'] == 'VOO', 'latest_close'].values[0]
+df_merge = df_merge.drop('Ticker', axis=1)
+df_merge['voo_latest_price'] = voo_latest_price 
+df_merge['s_p/l'] = (df_merge['latest_close'] - df_merge['Bought Price']) * df_merge.Quantity
+df_merge['v_p/l'] = (df_merge['voo_latest_price'] - df_merge['voo_close'])
+df_merge['total_val'] = df_merge['Bought Price'] * df_merge.Quantity
+#%%
+df_merge.to_csv(r'C:\Users\sdisawal\Desktop\Stocks\Code\csv\final_port.csv', header=True, index=False)
+
+
 
 #%%
+from plotly.offline import plot
+import plotly.figure_factory as ff
+#%%
+fig = ff.create_table(df_merge)
+#%%
+fig = px.histogram(data_frame=df_merge
+     ,x = 'Symbol'
+     ,y = ['s_p/l', 'v_p/l']
+     , title="Individual Stock vs Index"
+     , template='plotly'
+     )
+plot(fig, auto_open=True)
+
+#%%
+fig = px.histogram(data_frame=df_merge
+     ,x = 'Symbol'
+     ,y = ['s_p/l', 'v_p/l']
+     , title="Individual Stock vs Index"
+     , template='plotly'
+     )
+plot(fig, auto_open=True)
+
+#%%
+fig = px.pie(df_merge, values='total_val',
+             names='Stocks',
+             hole = 0.8,
+             title='Portfolio Distribution')
+plot(fig, auto_open=True)
 
 
 
