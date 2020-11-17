c7a239a9743644915136087afc21ba2c4573b4d0
sdisawal <disawalsagar>
2020-11-16 22:23:34 -0500

    Added comments and made code modular

--- snp_diff.py
+++ snp_diff.py
@@ -18,41 +18,37 @@ from alpha_vantage.fundamentaldata  import FundamentalData
 from alpha_vantage.timeseries  import TimeSeries 
 import pandas as pd
 import time
-from functools import reduce
-import numpy as np
+
 
 #%% Generate api key
 api_key = sk.fmp_api_key()
 
-#%%
+#%% Data Generation
 fd = FundamentalData(key=api_key, output_format='pandas')
 ts = TimeSeries(key=api_key, output_format='pandas')
 
 
 #%% 
-p_df = pd.read_csv(r'C:\Users\sdisawal\python_projects\focusedstock\rbh.csv', index_col='Symbol')
+portfolio_df = pd.read_csv(r'C:\Users\sdisawal\python_projects\focusedstock\rbh.csv', index_col='Symbol')
 #del p_df["No"]
  
 #%%
 snp = ts.get_daily('VOO', outputsize='full')
-
-#%%
 snp[0].reset_index().to_csv(r'C:\Users\sdisawal\Desktop\Stocks\Code\csv\snp.csv', header=True, index=False)
 
 #%%
 snp = pd.read_csv(r'C:\Users\sdisawal\Desktop\Stocks\Code\csv\snp.csv')
-p_df = pd.read_csv(r'C:\Users\sdisawal\python_projects\focusedstock\rbh.csv')
+snp.rename(columns= {'4. close' : 'close'},inplace = True)
+portfolio_df = pd.read_csv(r'C:\Users\sdisawal\python_projects\focusedstock\rbh.csv')
 #%%
-
-dt_fltr = p_df.loc[:,'Date']
-v_date = snp[snp['date'].isin(dt_fltr)][['date','4. close']].reset_index(drop=True)
-v_date.rename(columns= {'4. close' : 'close'},inplace = True)
-v_date['Ticker'] = 'VOO'
+dt_fltr = portfolio_df['Date']
+voo_df = snp[snp['date'].isin(dt_fltr)][['date','close']].reset_index(drop=True)
+voo_df['Ticker'] = 'VOO'
 #%% Today's return of all stocks
 
-tickers = set((p_df["Symbol"]).to_list())
+tickers = set((portfolio_df["Symbol"]).to_list())
 tickers.add('VOO')
-t_df = pd.DataFrame()
+ohlc_all_tickers_df = pd.DataFrame()
 i = 1
 for ticker in tickers:
         print("Getting values for {}, {}".format(i, ticker))
@@ -61,26 +57,23 @@ for ticker in tickers:
             i = 0
         data, metadata = ts.get_daily(symbol=ticker, outputsize='full')
         data['Ticker'] = ticker
-        t_df = t_df.append(data)
+        ohlc_all_tickers_df  = ohlc_all_tickers_df .append(data)
         i= i+1
 
 #%%
-t_df.rename(columns= {'4. close' : 'close'},inplace = True)
-#%%
-t_df.reset_index().to_csv(r'C:\Users\sdisawal\Desktop\Stocks\Code\csv\stock_Values.csv', header=True, index=False)
+ohlc_all_tickers_df.reset_index().to_csv(r'C:\Users\sdisawal\Desktop\Stocks\Code\csv\stock_Values.csv', header=True, index=False)
 
 #%%
-t_df = pd.read_csv(r'C:\Users\sdisawal\Desktop\Stocks\Code\csv\stock_Values.csv')
-t_df = t_df.rename(columns= {'4. close' : 'close'}).reset_index(drop=True)
+ohlc_all_tickers_df = pd.read_csv(r'C:\Users\sdisawal\Desktop\Stocks\Code\csv\stock_Values.csv')
+ohlc_all_tickers_df.rename(columns= {'4. close' : 'close'},inplace = True)
 
 #%%
-#print(p_df.columns)
-p_df = p_df[['Stocks', 'Symbol', 'Quantity','Bought Price','Date']]
-p_df = p_df.rename(columns = {'Date': 'org_date'})
-t_df_VOO = t_df[t_df['Ticker'] == 'VOO'][['Ticker','date','close']]
-t_df_VOO = t_df_VOO.rename(columns = {'close': 'voo_close', 'Ticker': 'voo_Ticker'})
-df_merge1 = p_df.merge(t_df_VOO, left_on = ['org_date'], right_on=['date'], how='left')
-#df_merge1.rename(columns= {'Date' : 'Date_stock', },inplace = True)
+#Figure out VOO stock price on the day of stock bought date
+portfolio_df = portfolio_df[['Stocks', 'Symbol', 'Quantity','Bought Price','Date']]
+portfolio_df.rename(columns = {'Date': 'bought_date'},inplace = True)
+t_df_VOO = ohlc_all_tickers_df[ohlc_all_tickers_df['Ticker'] == 'VOO'][['Ticker','date','close']]
+t_df_VOO.rename(columns = {'close': 'voo_close', 'Ticker': 'voo_Ticker'},inplace = True)
+df_merge1 = portfolio_df.merge(t_df_VOO, left_on = ['bought_date'], right_on=['date'], how='left')
 df_merge1 = df_merge1.drop('date', axis=1)
 
 #%%
@@ -89,17 +82,22 @@ def lat_dts(df):
     df_srtd = df_1.groupby('Ticker').head(1).reset_index(drop=True)
     return df_srtd
 #%%
-t_df_srtd = lat_dts(t_df)[['date', 'close', 'Ticker']]
-t_df_srtd = t_df_srtd.rename(columns = {'date' : 'latest_date', 'close': 'latest_close'}).reset_index(drop=True)
+#getting the recent price of every stock
+ohlc_all_tickers_srtd_df = lat_dts(ohlc_all_tickers_df)[['date', 'close', 'Ticker']]
+ohlc_all_tickers_srtd_df.rename(columns = {'date' : 'latest_date', 'close': 'latest_close'},inplace = True)
 
 #%%
-df_merge = df_merge1.merge(t_df_srtd, left_on = ['Symbol'], right_on=['Ticker'], how='left')
-voo_latest_price = t_df_srtd.loc[t_df_srtd['Ticker'] == 'VOO', 'latest_close'].values[0]
+#join the table to have bought price, VOO price on the bought date and recent price of stock and VOO in the same table
+df_merge = df_merge1.merge(ohlc_all_tickers_srtd_df, left_on = ['Symbol'], right_on=['Ticker'], how='left')
+voo_latest_price = ohlc_all_tickers_srtd_df.loc[ohlc_all_tickers_srtd_df['Ticker'] == 'VOO', 'latest_close'].values[0]
 df_merge = df_merge.drop('Ticker', axis=1)
 df_merge['voo_latest_price'] = voo_latest_price 
+df_merge['total_val'] = df_merge['Bought Price'] * df_merge.Quantity
+#%%
+#calculating the difference between recent and bough price
 df_merge['s_p/l'] = (df_merge['latest_close'] - df_merge['Bought Price']) * df_merge.Quantity
 df_merge['v_p/l'] = (df_merge['voo_latest_price'] - df_merge['voo_close'])
-df_merge['total_val'] = df_merge['Bought Price'] * df_merge.Quantity
+
 #%%
 df_merge.to_csv(r'C:\Users\sdisawal\Desktop\Stocks\Code\csv\final_port.csv', header=True, index=False)
 
